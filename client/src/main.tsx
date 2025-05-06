5f4df08a712eaa6df2222190831875376f7854bd
nita9801 <187074331+nita9801@users.noreply.github.com>
2025-05-06 18:01:28 -0400

    added hook render and atlas

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,8 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import ReactDOM from 'react-dom/client'
+import 'bootstrap/dist/css/bootstrap.min.css'
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+import App from './App.js'
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <App />
 )
